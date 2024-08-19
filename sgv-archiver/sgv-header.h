ec028afc65e92bf91c4549317fa03556b5a6d2b5
Jeff Adams <jeff@jamodeler.com>
2024-08-19 19:33:59 -0400

    renamed gav-archiver to sgv-archiver (Seekable Game Vault).  First fully working version.

---
+++ sgv-archiver/sgv-header.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <inttypes.h>
+
+struct sgv_header{
+
+	char			magic[4];							// SGV!
+	char			version_major;
+	char			version_minor;
+	char			index_hash_function;				// 0 = No Hash
+														// 1 = Multiplication 1 : hash = ( hash + modifier * value )  % index_size;
+														// 2 = Multiplication 2 : hash = ( hash + ( modifier * value ) + value ) % index_size;
+	uint32_t		index_hash_function_modifier;
+	uint32_t		index_size;
+	uint32_t		index_entries;
+
+} ;
