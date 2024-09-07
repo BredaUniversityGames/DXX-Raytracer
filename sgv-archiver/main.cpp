38167668770842fe2900021bea8936e8bfb482f1
Jeff Adams <jeff@jamodeler.com>
2024-09-07 16:22:35 -0400

    Updates to sgv-archiver

--- sgv-archiver/main.cpp
+++ sgv-archiver/main.cpp
@@ -8,12 +8,14 @@
 #include <limits>
 #include <iostream>
 #include <fstream>
+#include <string>
 #include "sgv-header.h"
 #include "sgv-index-entry.h"
+#include "sgv-utils.h"
 
 int main(int argc, char* argv[])
 {
-	printf("SGV Archiver\n");
+	//printf("SGV Archiver\n");
 
 	long long headerSize = 19;		// manually set the header size of sgv as sizeof(header) will return 20 due to padding;
 	
@@ -21,21 +23,19 @@ int main(int argc, char* argv[])
 	char* arg_output = nullptr;
 
 	int index_size_multiplier = 2;
+	int passes = 25;
 
-	uint32_t prime_list[] = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997 };// , 1009, 1013, 1019, 1021, 1031, 1033, 1039, 1049, 1051, 1061, 1063, 1069, 1087, 1091, 1093, 1097, 1103, 1109, 1117, 1123, 1129, 1151, 1153, 1163, 1171, 1181, 1187, 1193, 1201, 1213, 1217, 1229, 1231, 1237, 1249, 1259, 1277, 1279, 1289, 1291, 1297, 1301, 1303, 1319, 1321, 1327, 1361, 1367, 1373, 1381, 1399, 1409, 1423, 1427, 1429, 1433, 1439, 1447, 1451, 1453, 1459, 1471, 1481, 1487, 1493, 1499, 1511, 1523, 1531, 1543, 1549, 1553, 1559, 1567, 1571, 1579, 1583, 1597, 1601, 1607, 1609, 1619, 1627, 1637, 1657, 1663, 1667, 1669, 1693, 1697, 1699, 1709, 1721, 1723, 1733, 1747, 1753, 1759, 1777, 1783, 1787, 1801, 1811, 1823, 1831, 1847, 1861, 1867, 1871, 1873, 1877, 1879, 1889, 1901, 1907, 1913, 1931, 1933, 1949, 1973, 1979, 1987, 1993, 1997, 1999, 2003, 2011, 2017, 2027, 2029, 2039, 2053, 2063, 2069, 2081, 2083, 2087, 2089, 2099, 2111, 2113, 2129, 2131, 2137, 2141, 2143, 2153, 2161, 2179, 2203, 2207, 2213, 2221, 2237, 2239, 2243, 2251, 2267, 2269, 2273, 2281, 2287, 2293, 2297, 2309, 2311, 2339, 2341, 2357, 2371, 2377, 2381, 2383, 2387, 2393, 2399, 2411, 2417, 2423, 2437, 2441, 2447, 2459, 2467, 2473, 2477, 2503, 2521, 2531, 2539, 2549, 2551, 2557, 2579, 2591, 2593, 2609, 2617, 2621, 2633, 2647, 2657, 2659, 2663, 2671, 2677, 2687, 2689, 2693, 2699, 2707, 2711, 2719, 2729, 2731, 2741, 2749, 2753, 2767, 2777, 2789, 2791, 2797, 2801, 2803, 2819, 2833, 2837, 2843, 2851, 2857, 2861, 2879, 2887, 2897, 2903, 2909, 2917, 2927, 2939, 2953, 2957, 2969, 2971, 2999, 3001, 3011, 3019, 3023, 3037, 3041, 3049, 3061, 3067, 3079, 3083, 3087, 3109, 3119, 3121, 3137, 3163, 3167, 3169, 3181, 3187, 3191, 3203, 3209, 3217, 3221, 3229, 3251, 3257, 3259, 3271, 3299, 3307, 3313, 3319, 3329, 3331, 3343, 3347, 3359, 3361, 3371, 3373, 3389, 3391, 3407, 3413, 3433, 3449, 3457, 3461, 3463, 3467, 3471, 3473, 3487, 3491, 3499, 3511, 3517, 3527, 3539, 3541, 3547, 3557, 3571, 3581, 3583, 3593, 3607, 3617, 3623, 3631, 3637, 3643, 3659, 3671, 3673, 3677, 3691, 3697, 3701, 3709, 3719, 3727, 3733, 3739, 3749, 3761, 3767, 3769, 3779, 3787, 3793, 3797, 3803, 3821, 3827, 3833, 3847, 3851, 3853, 3863, 3877, 3881, 3889, 3893, 3907, 3911, 3917, 3923, 3929, 3931, 3943, 3947, 3967, 3989, 4001, 4003, 4007, 4013, 4019, 4027, 4049, 4051, 4057, 4073, 4079, 4091, 4093, 4099, 4111, 4127, 4129, 4133, 4139 };
-	//int prime_list_size = sizeof(prime_list) / sizeof(prime_list[0]);
-	int prime_list_size = 10;
+	uint32_t prime_list[] = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997 , 1009, 1013, 1019, 1021, 1031, 1033, 1039, 1049, 1051, 1061, 1063, 1069, 1087, 1091, 1093, 1097, 1103, 1109, 1117, 1123, 1129, 1151, 1153, 1163, 1171, 1181, 1187, 1193, 1201, 1213, 1217, 1229, 1231, 1237, 1249, 1259, 1277, 1279, 1289, 1291, 1297, 1301, 1303, 1319, 1321, 1327, 1361, 1367, 1373, 1381, 1399, 1409, 1423, 1427, 1429, 1433, 1439, 1447, 1451, 1453, 1459, 1471, 1481, 1487, 1493, 1499, 1511, 1523, 1531, 1543, 1549, 1553, 1559, 1567, 1571, 1579, 1583, 1597, 1601, 1607, 1609, 1619, 1627, 1637, 1657, 1663, 1667, 1669, 1693, 1697, 1699, 1709, 1721, 1723, 1733, 1747, 1753, 1759, 1777, 1783, 1787, 1801, 1811, 1823, 1831, 1847, 1861, 1867, 1871, 1873, 1877, 1879, 1889, 1901, 1907, 1913, 1931, 1933, 1949, 1973, 1979, 1987, 1993, 1997, 1999, 2003, 2011, 2017, 2027, 2029, 2039, 2053, 2063, 2069, 2081, 2083, 2087, 2089, 2099, 2111, 2113, 2129, 2131, 2137, 2141, 2143, 2153, 2161, 2179, 2203, 2207, 2213, 2221, 2237, 2239, 2243, 2251, 2267, 2269, 2273, 2281, 2287, 2293, 2297, 2309, 2311, 2339, 2341, 2357, 2371, 2377, 2381, 2383, 2387, 2393, 2399, 2411, 2417, 2423, 2437, 2441, 2447, 2459, 2467, 2473, 2477, 2503, 2521, 2531, 2539, 2549, 2551, 2557, 2579, 2591, 2593, 2609, 2617, 2621, 2633, 2647, 2657, 2659, 2663, 2671, 2677, 2687, 2689, 2693, 2699, 2707, 2711, 2719, 2729, 2731, 2741, 2749, 2753, 2767, 2777, 2789, 2791, 2797, 2801, 2803, 2819, 2833, 2837, 2843, 2851, 2857, 2861, 2879, 2887, 2897, 2903, 2909, 2917, 2927, 2939, 2953, 2957, 2969, 2971, 2999, 3001, 3011, 3019, 3023, 3037, 3041, 3049, 3061, 3067, 3079, 3083, 3087, 3109, 3119, 3121, 3137, 3163, 3167, 3169, 3181, 3187, 3191, 3203, 3209, 3217, 3221, 3229, 3251, 3257, 3259, 3271, 3299, 3307, 3313, 3319, 3329, 3331, 3343, 3347, 3359, 3361, 3371, 3373, 3389, 3391, 3407, 3413, 3433, 3449, 3457, 3461, 3463, 3467, 3471, 3473, 3487, 3491, 3499, 3511, 3517, 3527, 3539, 3541, 3547, 3557, 3571, 3581, 3583, 3593, 3607, 3617, 3623, 3631, 3637, 3643, 3659, 3671, 3673, 3677, 3691, 3697, 3701, 3709, 3719, 3727, 3733, 3739, 3749, 3761, 3767, 3769, 3779, 3787, 3793, 3797, 3803, 3821, 3827, 3833, 3847, 3851, 3853, 3863, 3877, 3881, 3889, 3893, 3907, 3911, 3917, 3923, 3929, 3931, 3943, 3947, 3967, 3989, 4001, 4003, 4007, 4013, 4019, 4027, 4049, 4051, 4057, 4073, 4079, 4091, 4093, 4099, 4111, 4127, 4129, 4133, 4139 };
 
-	printf("Parsing Arguments\n");
+	bool vault_has_been_split = false;		// tracks if an vault has been split so a number can be appended to vault name.
 
 	for (auto arg_index = 1; arg_index < argc; arg_index++)  // start at 1 because arg 0 is the executable path
 	{
-		if (strcmp("-h", argv[arg_index]) == 0)
+		if (strcmp("-h", argv[arg_index]) == 0 || strcmp("--help", argv[arg_index]) == 0)
 		{
 			// help argument detected
 			// print help screen and exit
-			printf("Help Screen");
-
+			printHelp();
 
 			return 0;
 		}
@@ -52,6 +52,19 @@ int main(int argc, char* argv[])
 			arg_index++;
 			if (arg_index < argc)
 				arg_output = argv[arg_index];
+		}
+		else if (strcmp("-p", argv[arg_index]) == 0)
+		{
+			// next argument should be the number of passes
+			arg_index++;
+			if (arg_index < argc)
+			{
+				if (atoi(argv[arg_index]) != 0)
+				{
+					passes = atoi(argv[arg_index]);
+					passes = std::min(passes, 500);
+				}
+			}
 		}
 		else
 			printf("Unknown Argument: %s\n", argv[arg_index]);
@@ -82,7 +95,10 @@ int main(int argc, char* argv[])
 
 	// Validate the output path
 	std::filesystem::path output_path = std::filesystem::path(arg_output);
+	std::filesystem::path output_path_extension = output_path.extension();
+	std::filesystem::path output_path_wo_extension = output_path.replace_extension("");
 	std::filesystem::path output_path_parent = output_path.parent_path();
+	
 	printf("Output parent directory: %s\n", output_path_parent.generic_string().c_str());
 
 	if (!std::filesystem::exists(output_path_parent))
@@ -122,6 +138,8 @@ int main(int argc, char* argv[])
 	// iterate over the list of files to add them to archives splitting them every 4GB.
 	constexpr uint32_t max_file_size = std::numeric_limits<uint32_t>::max();
 
+	int archive_count = 0;
+
 	while (!files.empty())
 	{
 		printf("Remaining files to be archived: %zd\n", files.size());
@@ -148,6 +166,7 @@ int main(int argc, char* argv[])
 			if (current_archive_size + (sizeof(sgv_index_entry) * index_size_multiplier) + current_file_size < max_file_size)
 			{
 				// there is room to fit the new file into the archive
+				current_data_size += current_file_size;
 				files_for_current_archive.push_back(current_file);	// TODO does current_file need to be deep copied here or is the reference good enough?
 				files.pop_back();
 			}
@@ -156,6 +175,7 @@ int main(int argc, char* argv[])
 				// there is not enough room to fit the new file into the archive
 				printf("Current Archive Full, Starting new...");
 				current_archive_filled = true;
+				vault_has_been_split = true;
 			}
 		}
 
@@ -164,24 +184,20 @@ int main(int argc, char* argv[])
 		// determine the best hash modifier value to use for these index entries that minimize collisions
 		uint32_t modifier_min = 1;
 		uint32_t modifier_max = 1024;
-		//uint32_t modifier_current = 0;
 
 		uint32_t modifier_selected = 0;
-		uint32_t modifier_selected_collisions = std::numeric_limits<uint32_t>::max();
+		double modifier_selected_spread_score = 1000000000.0;// std::numeric_limits<double>::max();
 
 		// loop through every hash modifier value and test build the index to test for collisions.  The one with the fewest collisions is selected.  
 		// If a modifier value is found to have no collisions it is chosen immediately and the search is ended.
-
-		
 		
 		//for (uint32_t modifier_current = modifier_min; modifier_current < modifier_max && modifier_selected_collisions != 0; modifier_current++)
-		for (uint32_t modifier_current_index = 0; modifier_current_index < prime_list_size && modifier_selected_collisions != 0; modifier_current_index++)
+		for (uint32_t modifier_current_index = 0; modifier_current_index < passes && modifier_selected_spread_score != 0.0; modifier_current_index++)
 		{
+			printf("Optimizing Hash: Pass# %zd/%zd \r", modifier_current_index + 1, passes);
+			fflush(stdout);
+
 			uint32_t modifier_current = prime_list[modifier_current_index];
-
-			printf("Trying modifier :%d\n", modifier_current);
-
-			uint32_t modifier_current_collisions = 0;
 
 			// allocate space for the current test index
 			auto index_size = files_for_current_archive.size() * index_size_multiplier;
@@ -192,12 +208,7 @@ int main(int argc, char* argv[])
 				std::filesystem::path current_path = files_for_current_archive[file_index];
 				std::string file_string = std::filesystem::relative(current_path, std::filesystem::path(arg_input)).generic_string();
 					
-				uint32_t file_string_hash = 0;
-
-				for (auto char_index = 0; char_index < file_string.size(); char_index++) {
-
-					file_string_hash = (file_string_hash * modifier_current + file_string.at(char_index)) % index_size;
-				}
+				uint32_t file_string_hash = hashString(file_string, 1, index_size, modifier_current);
 
 				// starting with the hashed index, find a place in the index for this file.
 				bool found_place = false;
@@ -213,7 +224,6 @@ int main(int argc, char* argv[])
 					else
 					{
 						// index slot is already used
-						modifier_current_collisions++;
 
 						// move index to next slot
 						file_string_hash = ( file_string_hash + 1 ) % index_size;
@@ -224,27 +234,36 @@ int main(int argc, char* argv[])
 			// files are all placed in the index
 
 			// see if this index is better than the current best index
-			if (modifier_current_collisions < modifier_selected_collisions)
+			double modifier_current_spread_score = calculateSpread(index_entries,index_size);
+
+			if (modifier_current_spread_score < modifier_selected_spread_score)
 			{
 				modifier_selected = modifier_current;
-				modifier_selected_collisions = modifier_current_collisions;
+				modifier_selected_spread_score = modifier_current_spread_score;
 			}
 
+
 			delete[] index_entries;
-			printf("Current Hash Modifier: %d, Collisions: %d\n", modifier_current, modifier_current_collisions);
-			printf("Current Best Hash Modifier: %d, Collisions: %d\n", modifier_selected, modifier_selected_collisions);
 		}
 
-		printf("Selected Hash Modifier: %d, Collisions: %d\n", modifier_selected, modifier_selected_collisions);
+		printf("Selected Hash Modifier: %d, Spread Score: %f\n", modifier_selected, modifier_selected_spread_score);
 
 		// create the actual archive now
+		printf("Writing Vault\n");
 		uint32_t total_file_size = 0;
+		std::filesystem::path final_output_path;
 
-		std::ofstream sgv_fs(output_path, std::ios::out | std::ios::binary);
+
+		if(vault_has_been_split)
+			final_output_path = output_path_wo_extension.string() + std::to_string(archive_count) + output_path_extension.string(); 
+		else
+			final_output_path = output_path_wo_extension.string() + output_path_extension.string();
+
+		std::ofstream sgv_fs(final_output_path, std::ios::out | std::ios::binary);
 
 		if (!sgv_fs)
 		{
-			printf("Error opening file for writing: %s\n", output_path.generic_string().c_str());
+			printf("Error opening file for writing: %s\n", final_output_path.generic_string().c_str());
 			return 1;
 		}
 
@@ -261,7 +280,6 @@ int main(int argc, char* argv[])
 		header.index_hash_function = 1;										// set hash_function to 1 (multiplication hash 1), NOTE may allow other values in future.
 		header.index_hash_function_modifier = modifier_selected;
 
-		//sgv_fs.write((char*)&header, sizeof(header));
 		sgv_fs.write((char*)&header.magic, sizeof(char) * 4);
 		sgv_fs.write((char*)&header.version_major, sizeof(char));
 		sgv_fs.write((char*)&header.version_minor, sizeof(char));
@@ -284,12 +302,7 @@ int main(int argc, char* argv[])
 			std::string file_string = std::filesystem::relative(current_path, std::filesystem::path(arg_input)).generic_string();
 			auto current_file_size = std::filesystem::file_size(current_path);
 
-			uint32_t file_string_hash = 0;
-
-			for (auto char_index = 0; char_index < file_string.size(); char_index++) {
-
-				file_string_hash = (file_string_hash * modifier_selected + file_string.at(char_index)) % index_size;
-			}
+			uint32_t file_string_hash = hashString(file_string, 1, index_size, modifier_selected);
 
 			// starting with the hashed index, find a place in the index for this file.
 			bool found_place = false;
@@ -343,10 +356,11 @@ int main(int argc, char* argv[])
 
 		
 		sgv_fs.close();
+
+		archive_count++;
 		
 	}
 	
-
 	return 0;
 	
 }
---
+++ sgv-archiver/sgv-utils.cpp
@@ -0,0 +1,103 @@
+#include <vector>
+#include <numeric>
+#include <cmath>
+
+#include "sgv-utils.h"
+#include "sgv-index-entry.h"
+
+void printHelp()
+{
+    printf("\nsgv-archiver v1.0\n\n");
+
+    printf("============\n");
+    printf("    HELP\n");
+    printf("============\n");
+
+    printf("Takes an input directory and creates a seekable game vault (SGV) file.\n\n");
+
+    printf("\t-h, --help\tPrint this help screen\n\n");
+    printf("\t-i\t\tThe directory to use as the source of the content to be included in the vault.  All files and folders inside this directory will be included in the vault.\n");
+    printf("\t\t\tex: C:/vault-content/\n");
+    printf("\t\t\t( REQUIRED )\n\n");
+    printf("\t-o\t\tThe file name of the vault to be created.\n");
+    printf("\t\t\tex: C:/vault-output/vault.sgv\n");
+    printf("\t\t\t( REQUIRED )\n\n");
+    printf("\t-p\t\tNumber of passes to optimize hashing algorithm for faster seeking.\n");
+    printf("\t\t\t( Default=25 )\n\n");
+}
+
+uint32_t hashString(const std::string& string, char hash_function, uint32_t size, uint32_t modifier)
+{
+    uint32_t string_hash = 0;
+
+    if (hash_function == 0)
+        return 0;
+
+    else if (hash_function == 1)
+    { 
+        for (auto char_index = 0; char_index < string.size(); char_index++) {
+
+            string_hash = (string_hash * modifier + string.at(char_index)) % size;
+        }
+    }
+
+    return string_hash;
+}
+
+// Function to calculate the spread score
+
+double calculateSpread(const sgv_index_entry* index_entries, size_t index_size) {
+
+    std::vector<int> filled_indices;
+
+    // Collect indices of filled entries
+    for (int i = 0; i < index_size; ++i) {
+
+        if (index_entries[i].key[0] != 0) { // Assuming 0 indicates an empty entry
+
+            filled_indices.push_back(i);
+            //printf("#");
+        }
+        //else
+            //printf(" ");
+    }
+    printf("\n");
+
+    if (filled_indices.size() < 2)
+    {
+        return 0.0; // Not enough entries to calculate spread
+    }
+
+    // Calculate distances between consecutive filled entries
+
+    std::vector<int> distances;
+    if (filled_indices[0] > 0)
+    {
+        distances.push_back(filled_indices[0]); // Distance from start of array to first filled entry
+    }
+
+    for (int i = 1; i < filled_indices.size(); ++i)
+    {
+        distances.push_back(filled_indices[i] - filled_indices[i - 1] - 1); // Subtract 1 to count only empty spaces
+    }
+
+    if (filled_indices.back() < index_size - 1)
+    {
+        distances.push_back(index_size - 1 - filled_indices.back()); // Distance from last filled entry to end of array
+    }
+
+    // Calculate the mean distance
+    double meanDistance = std::accumulate(distances.begin(), distances.end(), 0.0) / distances.size();
+
+    // Calculate the standard deviation of distances
+    double variance = 0.0;
+    for (int distance : distances) 
+    {
+        variance += std::pow(distance - meanDistance, 2);
+    }
+
+    variance /= distances.size();
+    double stdDeviation = std::sqrt(variance);
+
+    return stdDeviation;
+}
